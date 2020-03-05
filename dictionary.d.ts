91f0181f3745760ecaaa7e7914db37cb8d5d2090
Raul Macarie <raul@macarie.me>
2020-03-05 21:32:53 +0100

    Add `dictionary` typings

---
+++ dictionary.d.ts
@@ -0,0 +1,12 @@
+/**
+ * `compatto`'s standard dictionary.
+ * Even if it is the standard one it must be explicitly set by the user.
+ *
+ * @example
+ *  import { dictionary } from 'compatto/dictionary'
+ *  import { compatto } from 'compatto'
+ *
+ *  compatto({ dictionary })
+ *  => { compress, decompress }
+ */
+export const dictionary: string[]
